1179d9895a951973bbf400ac9db6f210d07c3fd9
YunaAnn <ann.grzywnowicz@gmail.com>
2020-10-22 21:47:21 +0200

    Palindrome

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 1. Name Swap
      - EXAMPLE:
-        'Doe Jane' -> 'Jane Doe'
-        'Nowak Jan' -> 'Jan Nowak'
+         1. 'Doe Jane' -> 'Jane Doe'
+         2. 'Nowak Jan' -> 'Jan Nowak'
+
+2. Palindrome 
+      - EXAMPLE PALIDROMES:
+         1. 'Never odd or even' 
+         2. 'Do geese see God?'  
--- algorithms.js
+++ algorithms.js
@@ -1,27 +1,41 @@
-function nameSwap(/*str*/)
+function nameSwap()
 {
     //  EXAMPLE
         // 'Doe Jane' -> 'Jane Doe'
         // 'Nowak Jan' -> 'Jan Nowak'
 
     /*
-    var stringToArray = str.split(' ');
+    var stringToArray = nameSwapString.split(' ');
     var reverseArray = stringToArray.reverse();
     var reverseString = reverseArray.join(' ');
-    return reverseString;
+    document.getElementById("swapNameResult").innerHTML = reverseString;
     */
 
     //Indexes
     /*
-    var array = str.split(' ');
-    return array[1] + ' ' + array[0];
+    var array = nameSwapString.split(' ');
+    var result = array[1] + ' ' + array[0];
+    document.getElementById("swapNameResult").innerHTML = result;
     */
-
-    //One line 
-    //return str.split(' ').reverse().join(' ');
 
     var nameSwapString = document.getElementById("nameSwap").value;
     
     var nameSwapResult = nameSwapString.split(' ').reverse().join(' ');
     document.getElementById("swapNameResult").innerHTML = nameSwapResult;
-}    
+}   
+
+function palindrome()
+{
+    //EXAMPLE PALIDROMES:
+    //1. 'Never odd or even' 
+    //2. 'Do geese see God?'
+    
+    var palindromeString = document.getElementById("palindrome").value;
+
+    var palindromeRemovedNonWordsCharacters = palindromeString.replace(/\W/g, '');
+    var caseInsensitiveString = palindromeRemovedNonWordsCharacters.toLowerCase();
+    var reversedString = caseInsensitiveString.split('').reverse().join('');
+    var palindromeResult = caseInsensitiveString === reversedString;
+
+    document.getElementById("palindromeResult").innerHTML = palindromeResult;
+}
