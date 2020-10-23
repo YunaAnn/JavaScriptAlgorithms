5bc9a45e2109f405c69cb328ab8617a83a4676b0
YunaAnn <ann.grzywnowicz@gmail.com>
2020-10-23 23:22:43 +0200

    Algorithms - Reverse String, Repeat String

--- README.md
+++ README.md
@@ -7,10 +7,20 @@
 
 1. Name Swap
      - EXAMPLE:
-         1. 'Doe Jane' -> 'Jane Doe'
-         2. 'Nowak Jan' -> 'Jan Nowak'
+         1. Doe Jane -> Jane Doe
+         2. Nowak Jan -> Jan Nowak
 
 2. Palindrome 
       - EXAMPLE PALIDROMES:
-         1. 'Never odd or even' 
-         2. 'Do geese see God?'  
+         1. Never odd or even. 
+         2. Do geese see God? 
+
+3. Reverse String
+    - EXAMPLE </h3>
+        1. cat -> tac
+        2. lovely -> ylevol
+
+4. Repeat String
+      -  EXAMPLE:
+        1. cat, 5 -> catcatcatcatcat
+        2. rain, 4' -> rainrainrainrain
--- algorithms.js
+++ algorithms.js
@@ -39,3 +39,54 @@ function palindrome()
 
     document.getElementById("palindromeResult").innerHTML = palindromeResult;
 }
+
+function reverseString()
+{   
+    //EXAMPLE:
+    //1. cat, 5 -> catcatcatcatcat
+    //2. rain, 4' -> rainrainrainrain
+
+    var reverseString = document.getElementById("reverseString").value;
+    
+    var reverseStringResult = reverseString.split('').reverse().join('');
+    document.getElementById("reverseStringResult").innerHTML = reverseStringResult;
+
+}
+
+
+function repeatString()
+{
+    //EXAMPLE
+    //'cat', 5 -> 'catcatcatcatcat'
+    //'rain, 4' -> 'rainrainrainrain' 
+
+    var str = document.getElementById("repeatStringString").value;
+    var num = document.getElementById("repeatStringNumber").value;
+
+    var finalString = '';
+
+    if(num <= 0)
+    {
+        finalString = ' Error. Incorrect data. ';
+    }
+
+    //for loop
+    for (var index = 1; index <=num; index++)
+    {
+        finalString += str; 
+    }
+
+    //or while loop
+    /*
+    while (num > 0) 
+    {
+        finalString += str; 
+        num--;
+    }
+    */
+
+    document.getElementById("repeatStringResult").innerHTML = finalString;
+  
+}
+
+
